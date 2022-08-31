6b200a208d33575eb0d55f2312f886ad82653546
ToniNov <antoni.novik@yandex.ru>
2022-08-31 14:48:41 +0300

    Types for users api

--- src/api/users-api.ts
+++ src/api/users-api.ts
@@ -1,17 +1,16 @@
 import {APIResponseType, instance} from "./api";
 
-
 export const usersApi = {
-    requestUsers (currentPage: number, pageSize: number) {
+    requestUsers(currentPage: number, pageSize: number) {
         return instance.get(`users?page=${currentPage}&count=${pageSize}`)
             .then(response => {
                 return response.data
             });
     },
-    follow (userId:number){
+    follow(userId: number) {
         return instance.post<APIResponseType>(`follow/${userId}`).then(res => res.data)
     },
-    unfollow (userId:number){
+    unfollow(userId: number) {
         return instance.delete(`follow/${userId}`).then(res => res.data) as Promise<APIResponseType>
     },
 }
