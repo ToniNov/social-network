5ba06d4fc33cde55681b102812c1ccdeed4d7cab
ToniNov <antoni.novik@yandex.ru>
2022-09-02 21:14:04 +0300

    ref

--- src/components/Header/HeaderContainer.tsx
+++ src/components/Header/HeaderContainer.tsx
@@ -2,36 +2,31 @@ import React from 'react';
 import Header from "./Header";
 import {connect} from "react-redux";
 import { logout} from "../../redux/auth-reducer";
-import {toggleIsFetching} from "../../redux/users-reducer";
 import {AppRootStateType} from "../../redux/redux-store";
 
 
-class HeaderContainer extends React.Component<AuthPropsType> {
+type MapPropsType = ReturnType<typeof mapStateToProps>
+type DispatchToPropsType = {
+    logout: () => void
+}
+
+type PropsType = MapPropsType & DispatchToPropsType
+
+class HeaderContainer extends React.Component<PropsType> {
 
     render() {
         return (
-            <Header  logout={this.props.logout} isAuth={this.props.isAuth} login={this.props.login}/>
+            <Header logout={this.props.logout}
+                    isAuth={this.props.isAuth}
+                    login={this.props.login}/>
         );
     }
 }
 
-type AuthPropsType = MapStateToPropsType & MapDispatchToPropsType
-
-type MapStateToPropsType = {
-    isAuth: boolean
-    login: string | null
-
-}
-
-type MapDispatchToPropsType = {
-    toggleIsFetching: (isFetching: boolean) => void
-    logout: () => void
-}
-
-const mapStateToProps = (state: AppRootStateType): MapStateToPropsType => ({
+const mapStateToProps = (state: AppRootStateType) => ({
     isAuth: state.auth.isAuth,
     login: state.auth.login
 })
 
-export default connect(mapStateToProps,
-    {toggleIsFetching,logout})(HeaderContainer)
+
+export default connect(mapStateToProps, {logout})(HeaderContainer)
