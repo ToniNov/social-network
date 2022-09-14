14907b5061326676c0d80f2c8361d3a3975c1aea
ToniNov <antoni.novik@yandex.ru>
2022-09-14 13:25:23 +0300

    update to R-d v6

---
+++ src/components/RoutersWrapper/RoutersWrapper.tsx
@@ -0,0 +1,10 @@
+
+export const PATH = {
+    Login: '/login',
+    Profile: '/profile',
+    Dialogs: '/dialogs',
+    Users: '/users',
+    News: '/news',
+    Music: '/music',
+    Setting: '/setting'
+}
